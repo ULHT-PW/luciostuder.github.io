c81fb1236ae24dd6533a0e190500617b0d3d8c62
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-06-13 01:43:59 +0100

    adicionei um script com um pequeno codigo

---
+++ static/script.js
@@ -0,0 +1,16 @@
+if (!localStorage.getItem('counter')) {
+      localStorage.setItem('counter', 0);
+}
+let counter = localStorage.getItem('counter');
+countElement = document.querySelector("#count");
+countElement.textContent = counter;
+
+const h1 = document.querySelector('h1');
+
+h1.onmouseover = () => {
+  h1.classList.add('colored');
+  counter++;
+  countElement.textContent = counter;
+  localStorage.setItem('counter', counter);
+}
+h1.onmouseout = () => h1.classList.remove('colored');

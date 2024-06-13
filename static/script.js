2da1f983a9aa0f35b7a821f40cef2685dff63b86
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-06-13 02:24:21 +0100

    reorganizei o JavaScript

--- static/script.js
+++ static/script.js
@@ -1,18 +1,21 @@
+const countElement = document.querySelector("#count");
+const h1 = document.querySelector('h1');
+
 if (!localStorage.getItem('counter')) {
       localStorage.setItem('counter', 0);
 }
+
 let counter = localStorage.getItem('counter');
-countElement = document.querySelector("#count");
 countElement.textContent = counter;
 
-const h1 = document.querySelector('h1');
-
 h1.onmouseover = () => {
-  h1.classList.add('colored');
   counter++;
   countElement.textContent = counter;
   localStorage.setItem('counter', counter);
+
+  h1.classList.add('colored');
 }
+
 h1.onmouseout = () => h1.classList.remove('colored');
 
 function saudar() {
